06b7068d210b77b1240f59074e85eeeacfa2f529
Mischback <dev@mischback.de>
2012-10-28 21:14:35 +0100

    Created basic skeleton for Alias class

--- alias.php
+++ alias.php
@@ -4,4 +4,13 @@
      *  @brief  Handles all alias related stuff
      */
 
+
+    /* handle basic stuff */
+    require_once('./lib/basic.php');
+
+
+    /* Gentlemen, start your engines! */
+    require_once('./engine/Alias.class.php');
+    require_once('./engine/Domain.class.php');
+
 ?>
--- engine/Alias.class.php
+++ engine/Alias.class.php
@@ -4,4 +4,94 @@
      *  @brief  TODO: insert smart description here!
      */
 
+
+    /* fetch our DAO */
+    require_once('./engine/AliasDAO.class.php');
+
+
+    /** @class  Alias
+     *  @brief  TODO: insert smart description here!
+     */
+    class Alias {
+
+        /** @brief  The alias' ID
+         */
+        private $alias_id = NULL;
+
+        /** @brief  Returns the alias' ID
+         *  @retval INT $alias_id
+         */
+        public function getAliasID() {
+            return $this->alias_id;
+        }
+
+
+        /** @brief  The alias' name
+         *
+         *  This is also the local-part of the alias
+         *
+         *  @todo: Check this against some RegEx to validate this!
+         */
+        private $alias_name = NULL;
+
+        /** @brief  Returns the alias' name
+         *  @retval STRING $alias_name
+         */
+        public function getAliasName() {
+            return $this->alias_name;
+        }
+
+
+        /** @brief  The alias' domain ID
+         *
+         *  This is the foreign key to domains
+         */
+        private $domain_id = NULL;
+
+        /** @brief  Returns the alias' domain ID
+         *  @retval INT $domain_id
+         */
+        public function getDomainID() {
+            return $this->domain_id;
+        }
+
+
+        /** brief   The alias' domain name
+         *
+         *  This is the actual domain name
+         */
+        private $domain_name = NULL;
+
+        /** @brief  Returns the alias' domain name
+         *  @retval STRING $domain_name
+         */
+        public function getDomainName() {
+            return $this->domain_name;
+        }
+
+
+        /** @brief  Returns the complete alias-address
+         *  @retval STRING
+         */
+        public function getAlias() {
+            return $this->alias_name.'@'.$this->domain_name;
+        }
+
+
+        /** @brief  The destination of this alias
+         *
+         *  Mail will be forwarded to this address
+         *
+         *  @todo: Check this against some RegEx to validate this!
+         */
+        private $alias_destination = NULL;
+
+        /** @brief  Returns the destination of this alias
+         *  @retval STRING $alias_destination
+         */
+        public function getAliasDestination() {
+            return $this->alias_destination;
+        }
+    }
+
 ?>
